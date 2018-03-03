16662e230776ccd8b0468a87603125dc526aa03f
czarny245 <czarny245@gmail.com>
2018-03-03 23:27:24 +0000

    Add API to the app

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,40 +1,87 @@
 import media
 import fresh_tomatoes
+import urllib2
+import json
 
-the_godfather = media.Movie("The Grandfather",
-                            "One of Hollywood's greatest critical and commercial successes, The Godfather gets everything right; not only did the movie transcend expectations, it established new benchmarks for American cinema.",
-                            "http://3.bp.blogspot.com/-kfFobXahgCI/T2e9e7R6uwI/AAAAAAAAAxY/tZtoOySAHKs/s1600/the-godfather-movie-poster-1020243893.jpg",
-                            "https://www.youtube.com/watch?v=5DO-nDW43Ik")
-print(the_godfather.storyline)
+key_api = 'fe09bc7261fdf7e9378f67ccbeeb84e1'
 
-twelve_monkeys = media.Movie("12 Monkeys",
-                             "The plot's a bit of a jumble, but excellent performances and mind-blowing plot twists make 12 Monkeys a kooky, effective experience.",
-                             "https://weekdaymatinee.files.wordpress.com/2012/05/12-monkeys-itunes.jpg",
-                             "https://www.youtube.com/watch?v=15s4Y9ffW_o")
+# Fetching Gladiator data from API service
+glad_obj = urllib2.urlopen("https://api.themoviedb.org/3/movie/98?api_key="+key_api)
+gladvid_obj = urllib2.urlopen("https://api.themoviedb.org/3/movie/98/videos?api_key="+key_api)
+glad_data = json.load(glad_obj)
+gladvid_data = json.load(gladvid_obj)
+for item in gladvid_data["results"]:
+    glad_yt_key = item["key"]
 
-cuckoo_nest = media.Movie("One Flew Over the Cuckoo's Nest",
-                           "A criminal pleads insanity after getting into trouble again and once in the mental institution rebels against the oppressive nurse and rallies up the scared patients. ",
-                           "http://4.bp.blogspot.com/-zocTOzAHvTY/TXlHS59pGOI/AAAAAAAAE28/w8C4g46mPds/s1600/one-flew-over-the-cuckoos-nest.jpg",
-                           "https://www.youtube.com/watch?v=OXrcDonY-B8")
+# Creating Gladiator object
+gladiator = media.Movie(glad_data["title"],
+	"https://image.tmdb.org/t/p/w600_and_h900_bestv2/"+glad_data["poster_path"],
+	"https://www.youtube.com/watch?v="+glad_yt_key)
 
-the_matrix = media.Movie("The Matrix",
-                         "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers. ",
-                         "http://1l5umu3rds2b2masq81ui5c2.wpengine.netdna-cdn.com/wp-content/uploads/2013/06/music-from-the-matrix-album.jpg",
-                         "https://www.youtube.com/watch?v=vKQi3bBA1y8")
+# Fetching Godfather data from API service
+god_obj = urllib2.urlopen("https://api.themoviedb.org/3/movie/238?api_key="+key_api)
+godvid_obj = urllib2.urlopen("https://api.themoviedb.org/3/movie/238/videos?api_key="+key_api)
+god_data = json.load(god_obj)
+godvid_data = json.load(godvid_obj)
+for item in godvid_data["results"]:
+    god_yt_key = item["key"]
 
-american_beauty = media.Movie("American Beauty",
-                              "A sexually frustrated suburban father has a mid-life crisis after becoming infatuated with his daughter's best friend. ",
-                              "http://eu.movieposter.com/posters/archive/main/65/MPW-32838",
-                              "https://www.youtube.com/watch?v=3ycmmJ6rxA8")
+# Creating Godfather object
+the_godfather = media.Movie(god_data["title"],
+	"https://image.tmdb.org/t/p/w600_and_h900_bestv2/"+god_data["poster_path"],
+	"https://www.youtube.com/watch?v="+god_yt_key)
 
-gladiator = media.Movie("Gladiator",
-                         "When a Roman General is betrayed, and his family murdered by an emperor's corrupt son, he comes to Rome as a gladiator to seek revenge. ",
-                         "https://fanart.tv/fanart/movies/98/movieposter/gladiator-52f1832e56737.jpg",
-                         "https://www.youtube.com/watch?v=owK1qxDselE")
+# Fetching 12monkeys data from API service
+tw_obj = urllib2.urlopen("https://api.themoviedb.org/3/movie/63?api_key="+key_api)
+twvid_obj = urllib2.urlopen("https://api.themoviedb.org/3/movie/63/videos?api_key="+key_api)
+tw_data = json.load(tw_obj)
+twvid_data = json.load(twvid_obj)
+for item in twvid_data["results"]:
+    tw_yt_key = item["key"]
 
+# Creating 12monkeys object
+twelve_monkeys = media.Movie(tw_data["title"],
+	"https://image.tmdb.org/t/p/w600_and_h900_bestv2/"+tw_data["poster_path"],
+	"https://www.youtube.com/watch?v="+tw_yt_key)
 
-movies = [twelve_monkeys, the_matrix, american_beauty, gladiator, cuckoo_nest]
+# Fetching Cuckoo data from API service
+cuck_obj = urllib2.urlopen("https://api.themoviedb.org/3/movie/510?api_key="+key_api)
+cuckvid_obj = urllib2.urlopen("https://api.themoviedb.org/3/movie/510/videos?api_key="+key_api)
+cuck_data = json.load(cuck_obj)
+cuckvid_data = json.load(cuckvid_obj)
+for item in cuckvid_data["results"]:
+    cuck_yt_key = item["key"]
+
+# Creating Cuckoo object
+cuckoo_nest = media.Movie(cuck_data["title"],
+	"https://image.tmdb.org/t/p/w600_and_h900_bestv2/"+cuck_data["poster_path"],
+	"https://www.youtube.com/watch?v="+cuck_yt_key)
+
+# Fetching Matrix data from API service
+mat_obj = urllib2.urlopen("https://api.themoviedb.org/3/movie/603?api_key="+key_api)
+matvid_obj = urllib2.urlopen("https://api.themoviedb.org/3/movie/603/videos?api_key="+key_api)
+mat_data = json.load(mat_obj)
+matvid_data = json.load(matvid_obj)
+for item in matvid_data["results"]:
+    mat_yt_key = item["key"]
+
+# Creating Matrix object
+the_matrix = media.Movie(mat_data["title"],
+	"https://image.tmdb.org/t/p/w600_and_h900_bestv2/"+mat_data["poster_path"],
+	"https://www.youtube.com/watch?v="+mat_yt_key)
+
+# Fetching American Beauty data from API service
+ame_obj = urllib2.urlopen("https://api.themoviedb.org/3/movie/14?api_key="+key_api)
+amevid_obj = urllib2.urlopen("https://api.themoviedb.org/3/movie/14/videos?api_key="+key_api)
+ame_data = json.load(ame_obj)
+amevid_data = json.load(amevid_obj)
+for item in amevid_data["results"]:
+    ame_yt_key = item["key"]
+
+# Creating American Beauty object
+american_beauty = media.Movie(ame_data["title"],
+	"https://image.tmdb.org/t/p/w600_and_h900_bestv2/"+ame_data["poster_path"],
+	"https://www.youtube.com/watch?v="+ame_yt_key)
+
+movies = [the_godfather, twelve_monkeys, the_matrix, american_beauty, gladiator, cuckoo_nest]
 fresh_tomatoes.open_movies_page(movies)
-
-#print(twelve_monkeys.storyline)
-#twelve_monkeys.show_trailer()
