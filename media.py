cb631e8c2bb22a8224275c974cc37094b4050e3f
czarny245 <czarny245@gmail.com>
2018-03-03 23:40:10 +0000

    final

--- media.py
+++ media.py
@@ -1,10 +1,9 @@
 import webbrowser
 class Movie():
     """This class contains movie related information"""
-    def __init__(self, movie_title, movie_storyline, poster_image,
+    def __init__(self, movie_title, poster_image,
                  trailer_youtube):
         self.title = movie_title
-        self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
