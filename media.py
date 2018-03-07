9bb9b8d1fe06e7b0a80c2c96bd9043583afc048c
czarny245 <czarny245@gmail.com>
2018-03-07 14:19:29 +0000

    Add comments in media.py

--- media.py
+++ media.py
@@ -1,8 +1,16 @@
 import webbrowser
 class Movie():
-    """This class contains movie related information"""
+    
     def __init__(self, movie_title, poster_image,
                  trailer_youtube):
+    
+    """This class contains movie related information
+        Args:
+            movie_title (str): Include the movie title
+            poster_image (str): Include a url to a movie poster image
+            trailer_youtube (str): Include a url to a movie trailer video
+
+        """
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
